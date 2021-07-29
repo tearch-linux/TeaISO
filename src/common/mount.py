79ff85323218fb7fb2588ee9fa0adadfd99d93c8
Ali Rıza KESKİN <ali.riza.keskin@pardus.org.tr>
2021-07-29 14:28:39 +0000

    mount operations

--- src/common/mount.py
+++ src/common/mount.py
@@ -1,10 +1,10 @@
 from utils import run, inf
 def mount_operations(rootfs):
-    for dir in ["dev", "sys", "proc", "run"]:
+    for dir in ["dev", "dev/pts", "sys", "proc", "run"]:
         run("mount --bind /{0} /{1}/{0}".format(dir,rootfs))
 
 def unmount_operations(rootfs):
-    for dir in ["dev", "sys", "proc", "run"]:
+    for dir in ["dev/pts", "dev", "sys", "proc", "run"]:
         while 0 == run("umount -lf -R /{}/{} 2>/dev/null".format(rootfs,dir)):
             True
 
