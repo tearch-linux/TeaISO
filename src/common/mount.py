8096d26abce0260c4c778c4753e433f2c1d1ad1a
ğ <@.>
2021-08-02 23:43:51 +0300

    mtab fix

--- src/common/mount.py
+++ src/common/mount.py
@@ -2,6 +2,8 @@ from utils import run, inf
 def mount_operations(rootfs):
     for dir in ["dev", "dev/pts", "sys", "proc", "run"]:
         run("mount --bind /{1} /{0}/{1} 2>/dev/null".format(rootfs,dir))
+    run("ln -s {0}/proc/self/fd {0}/dev/fd 2>/dev/null".format(rootfs),vital=False)
+    run("ln -s {0}/proc/self/mounts /etc/mtab")
 
 def unmount_operations(rootfs):
     for dir in ["dev/pts", "dev", "sys", "proc", "run"]:
