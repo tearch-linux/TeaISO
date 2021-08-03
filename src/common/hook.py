c3b50f018730bf5ac231eac55bf429af74b74c63
root <siz@e-posta.com>
2021-08-03 13:28:58 +0000

    fix

--- src/common/hook.py
+++ src/common/hook.py
@@ -2,7 +2,7 @@ import os
 from utils import inf, run, colorize
 def run_hook(settings,i):
     inf("==> Running: {}".format(colorize(i, 0)))
-    os.chmod(settings.profile + "/" + i, 0o755)
     run("cat \"{}\" > \"{}/tmp/hook\"".format(settings.profile+"/"+i,settings.rootfs))
+    os.chmod("\"{}/tmp/hook\"", 0o755)
     run("chroot \"{}\" /tmp/hook".format(settings.rootfs))
     run("rm -f \"{}/tmp/hook\"".format(settings.rootfs))
