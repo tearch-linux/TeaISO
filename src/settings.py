727022c0c8d11b0970eb07ed0666f1b5f0eee815
sulincix <parduscix@yandex.ru>
2022-04-27 23:00:26 +0000

    interactive fix

--- src/settings.py
+++ src/settings.py
@@ -51,7 +51,7 @@ def show(contents, packages):
             contents["repository"] if 'repository' in contents else 'N/A'))
     inf("\t{}: {}".format(colorize("Kernel cmdline", bold),
         contents["linux_args"] if 'linux_args' in contents else 'N/A'))
-    if interactive:
+    if interactive == "true":
         if input("Do you wanna continue? [Y/n]").lower()[0] != "y": 
             exit(1)
 
