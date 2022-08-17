c94738eb7d55750425720b58af35fda77d90a02f
𐰀𐰞𐰃:𐰺𐰃𐰔𐰀:𐰚𐰀𐰾𐰚𐰃𐰤 (𐰽𐰆𐰞𐰃𐰤𐰆𐰽) <parduscix@yandex.ru>
2022-08-17 06:36:33 +0000

    Update debian.sh

--- src/distro/debian.sh
+++ src/distro/debian.sh
@@ -86,7 +86,7 @@ customize_airootfs(){
 
 clear_rootfs(){
     run_in_chroot apt clean
-    run_in_chroot apt autoremove --purge
+    run_in_chroot apt autoremove --purge -y
     rm -rf $rootfs/var/lib/apt/lists || true
     find "$rootfs/var/log/" -type f | xargs rm -f  || true
 }
