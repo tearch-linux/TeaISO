13b821c20f5b2c546ac2acb064aa6c1d00e65eb4
sulincix <parduscix@yandex.ru>
2022-12-05 19:36:52 +0000

    apt install -f

--- src/distro/debian.sh
+++ src/distro/debian.sh
@@ -28,6 +28,7 @@ create_rootfs(){
     fi
     echo -e "#!/bin/sh\nexit 101" > "$rootfs"/usr/sbin/policy-rc.d
     chmod +x "$rootfs"/usr/sbin/policy-rc.d
+    run_in_chroot apt install -f -yq
     if [[ "" != "${keyring_package}" ]] ; then
         run_in_chroot apt install ${keyring_package} -yq
     fi
