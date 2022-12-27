3b2e55942d83d7ad5452f3b4a72000a8bb59c097
sulincix <parduscix@yandex.ru>
2022-12-27 19:48:51 +0000

    machine-id file removed

--- src/mkteaiso.py
+++ src/mkteaiso.py
@@ -265,8 +265,6 @@ if Stage().get() < 8:
 
 # Generate IsoWork
 if Stage().get() < 9:
-    with open("{}/etc/machine-id".format(settings.rootfs),"w") as f:
-        f.write(getoutput("dd if=/dev/urandom count=1 |& md5sum - | cut -f1 -d" ""))
     os.chdir(settings.workdir)
     for i in common.get("customize_isowork_pre", []):
         run(i)
